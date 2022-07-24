b9264b948debac32dff52ab46dc3f3b7ef168f85
Casper Green <cbkg11@gmail.com>
2022-07-24 17:35:36 +0200

    Fix mountpoint and use sudo to sync file

--- mount-aws-secret.sh
+++ mount-aws-secret.sh
@@ -1,8 +1,8 @@
 
-if [ ! mountpoint -q -- /secrets ]
+if ! mountpoint -q -- /secrets
 then
   mkdir -p /secrets 
   mount -t tmpfs tmpfs /secrets -o size=5m || exit 1
 fi
 
-node $(dirname -- $0)/index $1 $2
+sudo node $(dirname -- $0)/index $1 $2
